4f87042fd4f442b96340ae1d5f6eb1bbe2e44a7b
Jaan Jahilo <jaanjahilo@outlook.com>
2021-08-01 15:06:00 +0300

    Add docker-compose setup for test and knex instance

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello world!");
+import knex from 'knex';
+
+const instance = knex({
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        debug: false,
+        password: '',
+        database: 'test_database'
+    },
+});
+
+const main = async () => {
+    console.log(await instance.select(1));
+    process.exit(0);
+}
+
+await main();
